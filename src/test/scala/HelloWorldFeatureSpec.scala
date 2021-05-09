09c3404f3f8deb3fc8e395765ceba37b3f7c2c18
przemeksokoleoko <przemeksokoleoko@gmail.com>
2021-05-09 12:22:31 +0200

    Remove redundant test

--- src/test/scala/HelloWorldFeatureSpec.scala
+++ src/test/scala/HelloWorldFeatureSpec.scala
@@ -5,14 +5,9 @@ class HelloWorldFeatureSpec extends AnyFeatureSpec with GivenWhenThen {
     info("As a developer")
     info("I want to test Strings")
     Feature("A String") {
-        Scenario("When a user A provides 'Hello World' as input") {
+        Scenario("When a user provides 'Hello World' as input") {
             Then("The input must start with 'Hello'")
             assert("Hello World".startsWith("Hello"))
-        }
-
-        Scenario("When a user B provides 'Hello World' as input") {
-            Then("The input must end with 'World'")
-            assert("Hello World".endsWith("World"))
         }
     }
 }
